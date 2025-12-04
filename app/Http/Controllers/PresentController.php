e700a275412d244f238dc6673e8af051f170247c
PenyiDavid <penyaskodavid@gmail.com>
2025-12-04 14:56:37 +0100

    kommentek

--- app/Http/Controllers/PresentController.php
+++ app/Http/Controllers/PresentController.php
@@ -9,16 +9,25 @@ use App\Models\Present;
 class PresentController extends Controller
 {
     public function index(){
+        //present_type-ok lekérdezése az ajándékokkal együtt
         $present_types = PresentType::all()->load('presents');
+        //visszaadjuk a választ JSON formátumban, 200-as státusszal
         return response()->json($present_types, 200);
     }
 
     public function index2(){
+        //present_type-ok lekérdezése az ajándékokkal együtt, created_at és updated_at mezők láthatóak a típusoknál
         $present_types = PresentType::all()->makeVisible('updated_at', 'created_at')->load('presents');
         return response()->json($present_types, 200);
     }
     public function index3(){
+        //present_type-ok lekérdezése az ajándékokkal együtt, created_at és updated_at mezők láthatóak a típusoknál, valamint a created_at mező az ajándékoknál
+        //először lekérdezzük az összes típust és láthatóvá tesszük a created_at és updated_at mezőket
         $present_types = PresentType::all()->makeVisible('updated_at', 'created_at');
+        //típusokhoz tartozó ajándékok betöltése, majd az ajándékok created_at mezőjének láthatóvá tétele closure segítségével
+        //closure: névtelen függvény, amit átadunk egy másik függvénynek paraméterként (pl. itt az each-nek), 
+        //ami minden egyes elemre alkalmazza a closure-ben definiált műveletet
+        //each: egy kollekció minden elemére alkalmaz egy adott műveletet
         $present_types->each(function($type){
             $type->presents->makeVisible('created_at');
         });
@@ -26,24 +35,37 @@ class PresentController extends Controller
     }
 
     public function store(Request $request){
-        $request->validate([ //validation hibakód: 422
+        //új ajándék létrehozása és validálás
+        //validation hibakód: 422
+        //validációs szabályok, egyedi hibaüzenetek és mezőnevek megadása
+        //:attribute helyére a mező neve kerül
+        //exists: ellenőrzi, hogy a megadott érték létezik-e a megadott táblában és oszlopban
+        
+        $request->validate([ 
             'name' => 'required|string',
             'present_type_id' => 'required|exists:present_types,id'
         ],
         [
-            'name.required' => 'A name kitöltése kötelező',
-            'name.string' => 'A name egy szöveg',
-            'present_type_id.exists' => 'Az ajándék típus nem létezik' 
+            'required' => 'A(z) :attribute kitöltése kötelező',
+            'string' => 'A(z) :attribute szöveg típusú',
+            'exists' => 'A(z) :attribute típus nem létezik' 
         ],
         [
-            'name'=>'Ajándék neve'
+            'name'=>'Ajándék neve',
+            'present_type_id'=>'Ajándék típus'
         ]);
 
+        //hagyományos létrehozás
+        //ha tömb elemeit egyesével akarjuk megadni
         /*Present::create([
             'name' => $request['name'],
             'present_type_id' => $request['present_type_id']
         ]);*/
-
+        //egyszerűsített létrehozás
+        //ajándék létrehozása a kérés összes adatával, 
+        //reqest->all() csak akkor használható, ha a modellben a fillable mezők megfelelően vannak beállítva és a request-ben csak a megengedett mezők szerepelnek
+        //pl.: checkbox esetén érdemes külön kezelni a mezőket, mert ha nincs bejelölve, akkor nem kerül elküldésre a request-ben
+        
         $present = Present::create($request->all());
         return response()->json(['message'=>'Ajándék létrehozva', 'data'=> $present], 201);
     }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChildController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\http\Controllers\PresentController;
@@ -8,9 +9,17 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-
+//lekérdezi az összes ajándék típust az ajándékokkal együtt
 Route::get('/presents', [PresentController::class, 'index']);
+//lekérdezi az összes ajándék típust az ajándékokkal együtt, az ajándék típusok created_at és updated_at mezői láthatóak
 Route::get('/presents-type-ts', [PresentController::class, 'index2']);
+//lekérdezi az összes ajándék típust az ajándékokkal együtt, az ajándék típusok created_at és updated_at mezői, valamint az ajándékok created_at mezői láthatóak
 Route::get('/presents-all-ts', [PresentController::class, 'index3']);
 
+//új ajándék létrehozása
 Route::post('/new-present', [PresentController::class, 'store']);
+
+//gyerekek lekérdezése
+Route::get('/children', [ChildController::class, 'index']);
+//új gyerek létrehozása
+Route::post('/new-child', [ChildController::class, 'store']);
